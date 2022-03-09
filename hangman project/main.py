dac10e331f516cdee4a2fc6427d702fab8a294b9
Santiago Mendiola <santiago.mendiola01@utrgv.edu>
2022-03-09 10:42:09 -0600

    finished project

--- hangman project/main.py
+++ hangman project/main.py
@@ -10,13 +10,13 @@ answer_guesses = []
 
 # Determine which characters will be known before starting the game 
 for current_answer_character in answer_characters:
-  if re.search("^(A-Z)$",current_answer_character):
+  if re.search("^[A-Z]$",current_answer_character):
     answer_guesses.append(False)
   else:
     answer_guesses.append(True)
 
 # logic of playing the game
-guessed_letters:[]
+guessed_letters=[]
 num_of_incorrect_guesses = 0
 
 while num_of_incorrect_guesses < 5 and False in answer_guesses:
@@ -24,11 +24,11 @@ while num_of_incorrect_guesses < 5 and False in answer_guesses:
   print("Guessed Letters: ", end = "")
 
   for current_guessed_letters in guessed_letters:
-    print(f"{current_guessed_letter} ", end = "")
+    print(f"{current_guessed_letters} ", end = "")
 
   print()
 
-  print(f"Number of incorrect guesses remaining: {5 - num_incorrect_guesses}")
+  print(f"Number of incorrect guesses remaining: {5 - num_of_incorrect_guesses}")
 
   print()
 
@@ -45,7 +45,7 @@ while num_of_incorrect_guesses < 5 and False in answer_guesses:
 
   letter = letter.upper()
 
-  if letter not in guessed_letters and len(letter) == 1 and re.search("[A-Z]$",letter): 
+  if letter not in guessed_letters and len(letter) == 1 and re.search("^[A-Z]$",letter): 
     guessed_letters_insert_index = 0
 
     for current_guessed_letter in guessed_letters:
@@ -57,10 +57,10 @@ while num_of_incorrect_guesses < 5 and False in answer_guesses:
   guessed_letters.insert(guessed_letters_insert_index, letter) 
   if letter in answer_characters:
       #letter is in the puzzle
-      for current_answer_index in range(len(answer_characteristics)):
-        if letter == answer_characters[current_index]:
+      for current_answer_index in range(len(answer_characters)):
+        if letter == answer_characters[current_answer_index]:
           answer_guesses[current_answer_index] = True
-else:
+  else:
       num_of_incorrect_guesses += 1
 
 print()
